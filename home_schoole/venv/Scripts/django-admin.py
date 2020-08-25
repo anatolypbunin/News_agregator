39d8fe3b8f7f60cdc74ee1a94a44dabb474bc93d
anatoly_p_bunin <anatoly.p.bunin@gmail.com>
2020-08-25 15:58:15 +0300

    weather

---
+++ home_schoole/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\abaev\PycharmProjects\firstDjangoProject\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
